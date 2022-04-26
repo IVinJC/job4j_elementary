f1ff4079ea0b54d0dc242db5dfe422a747dd6c6b
Windows <ivinjc@gmail.com>
2022-04-26 18:57:04 +0300

    Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,51 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class X2Test {
+
+    @Test
+    public void whenA1B1C1X1Then40() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 30;
+        int rsl = X2.calc(a, b, c, x);
+        assertEquals(expected, rsl);
+    }
+    @Test
+    public void whenA0B1C1X1Then40() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 10;
+        int rsl = X2.calc(a, b, c, x);
+        assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C0X1Then40() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 15;
+        int rsl = X2.calc(a, b, c, x);
+        assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C1X0Then40() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 5;
+        int rsl = X2.calc(a, b, c, x);
+        assertEquals(expected, rsl);
+    }
+}
