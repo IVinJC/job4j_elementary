0bdba1524db1d5d885b703977ab72da80572738f
Windows <ivinjc@gmail.com>
2022-05-01 19:21:04 +0300

    Протеиновая диета [#156313 #295557]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -3,10 +3,13 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan = ivan * 3;
             nik = nik * 3;
             month++;
+            if (ivan == nik) {
+                break;
+            }
         }
         return month;
     }
--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -36,7 +36,7 @@ public class FitnessTest {
         int ivan = 90;
         int nik = 90;
         int result = Fitness.calc(ivan, nik);
-        int expected = 0;
+        int expected = 1;
         Assert.assertEquals(expected, result);
     }
 }
