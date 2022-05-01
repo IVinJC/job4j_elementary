9ee11d8269cdaa6b8e217dbb1e147112fd51ba2c
Windows <ivinjc@gmail.com>
2022-05-01 17:30:37 +0300

    Простое число [#171694 #295572]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,22 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
+        if (number < 2) {
+            return false;
+        }
         boolean prime = true;
-        for (int i = 2; i > Math.sqrt(number); i--) {
-            if (number % i == 0 || number == 1) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
                 prime = false;
                 break;
             }
         }
         return prime;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 200; i++) {
+            System.out.println(i + " " + check(i));
+        }
     }
 }
