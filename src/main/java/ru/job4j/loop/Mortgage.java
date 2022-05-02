840b32b1f7db52dbd373660750f924c7ab15fa50
Windows <ivinjc@gmail.com>
2022-05-02 16:02:50 +0300

    Ипотека [#156315 #295597]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double ostatok = 1;
-        while (ostatok > 0) {
-   ostatok = (amount * percent * 0.01 + amount) - salary;
-   amount = ostatok;
-   year++;
+        while (amount > 0) {
+            amount = (amount * percent * 0.01 + amount) - salary;
+            year++;
         }
         return year;
     }
