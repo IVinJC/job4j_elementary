ebfaf590325e104b5b55ea30560f09f057ab4cfa
Lenovo 320 <ivinjc@gmail.com>
2022-05-06 09:42:17 +0300

    Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        Book book2 = new Book();
+        Book book3 = new Book();
+        Book book4 = new Book();
+        book2.setName("Clean code");
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[3];
+            books[3] = books[0];
+            books[0] = bk;
+        }
+        for (int i = 0; i < books.length; i++) {
+            if ("Clean code".equals(books[i].getName())) {
+                System.out.println(books[i].getName());
+            }
+        }
+    }
+}
