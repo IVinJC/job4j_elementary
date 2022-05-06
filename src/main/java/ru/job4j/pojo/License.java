dc031b9f69044045f3c5d1e134e058900dced5b9
Lenovo 320 <ivinjc@gmail.com>
2022-05-06 11:56:26 +0300

    Сравнение моделей. Метод equals [#174893]

--- src/main/java/ru/job4j/pojo/License.java
+++ src/main/java/ru/job4j/pojo/License.java
@@ -52,4 +52,9 @@ public class License {
         License license = (License) o;
         return Objects.equals(owner, license.owner) && Objects.equals(model, license.model) && Objects.equals(code, license.code) && Objects.equals(created, license.created);
     }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
