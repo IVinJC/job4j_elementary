d14eb6f55ccd6a3fa6179f624a8f1138ce00fe3f
Lenovo 320 <ivinjc@gmail.com>
2022-05-11 11:44:53 +0300

    final [#334073 #299343]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int engine = new Airbus("A380").getName().equals("A380") ? 4 : COUNT_ENGINE;
+        int engine = "A380".equals(name) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + engine);
     }
 
