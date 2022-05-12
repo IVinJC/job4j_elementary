4b2801c95b55e269f466153d743ec863393efddc
Lenovo 320 <ivinjc@gmail.com>
2022-05-12 10:02:14 +0300

    Иерархия исключений и множественный catch. [#219368 #299095]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -4,9 +4,9 @@ public class UserStore {
 
     public static User findUser(User[] users, String login) throws UserNotFoundException {
         User user = null;
-        for (int i = 0; i < users.length; i++) {
-            if (users[i].getUsername().equals(login)) {
-                user = users[i];
+        for (User usr : users) {
+            if (usr.getUsername().equals(login)) {
+                user = usr;
                 return user;
             }
         }
