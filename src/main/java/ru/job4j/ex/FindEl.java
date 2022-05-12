f617720d065d20bcb1385f1e398059eac2a21a8e
Lenovo 320 <ivinjc@gmail.com>
2022-05-12 09:48:43 +0300

    Пользовательские исключения. [#219367 #299045]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,9 +3,6 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        if (key == null) {
-            throw new ElementNotFoundException("Element " + key + " should not be null");
-        }
         for (int i = 0; i < value.length; i++) {
             if (key.equals(value[i])) {
                 rsl = i;
